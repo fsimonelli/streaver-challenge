5d08db389d6a05f7d6d1df28706dc600c1636458
fsimonelli <franciscosimonelli6@gmail.com>
2025-05-20 20:17:44 -0300

    feat: toast for slow connections

--- src/app/ui/SearchBar.tsx
+++ src/app/ui/SearchBar.tsx
@@ -8,7 +8,7 @@ export default function SearchBar({ term, onSearchChange }: SearchProps) {
     <form className='m-4 mx-auto max-w-lg'>
       <div className='relative w-full'>
         <input
-          className='z-20 block w-full rounded border border-gray-300 border-s-gray-50 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:border-s-gray-700 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500'
+          className='z-20 block w-full rounded border border-gray-300 border-s-gray-50 bg-gray-800 p-2.5 text-sm focus:border-blue-500 focus:ring-blue-500'
           placeholder='Search posts by userId'
           onChange={onSearchChange}
           value={term}
--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,11 +1,12 @@
 const url = 'https://jsonplaceholder.typicode.com/posts';
 
-const fetcher = async (id: string) => {
+const fetcher = async (userId: string) => {
   let response;
-  if (id == 'all') {
+  await new Promise((res) => setTimeout(res, 5000)); //simulating delay
+  if (userId == 'all') {
     response = await fetch(url).then((res) => res.json());
   } else {
-    const searchURL = `${url}?userId=${id}`;
+    const searchURL = `${url}?userId=${userId}`;
     response = await fetch(searchURL).then((res) => res.json());
   }
   return response;
