b28b973d25f93f4d155c06cf1867fa60777dff46
fsimonelli <franciscosimonelli6@gmail.com>
2025-05-20 13:47:22 -0300

    fix: build error

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -3,7 +3,7 @@
 import useSWR from 'swr';
 import CardList from '@/app/ui/CardList';
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export default function Posts() {
   const { data, error, isLoading } = useSWR(
