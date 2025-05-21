8f1ea91d894e7882f0d1d8608d29639943285085
fsimonelli <franciscosimonelli6@gmail.com>
2025-05-21 11:25:43 -0300

    fix: removed form

--- src/app/ui/SearchBar.tsx
+++ src/app/ui/SearchBar.tsx
@@ -5,15 +5,13 @@ interface SearchProps {
 
 export default function SearchBar({ term, onSearchChange }: SearchProps) {
   return (
-    <form className='m-4 mx-auto max-w-lg'>
-      <div className='relative w-full'>
-        <input
-          className='z-20 block w-full rounded border border-gray-300 border-s-gray-50 bg-gray-800 p-2.5 text-sm focus:border-blue-500 focus:ring-blue-500'
-          placeholder='Search posts by userId'
-          onChange={onSearchChange}
-          value={term}
-        />
-      </div>
-    </form>
+    <div className='mx-aut relative m-4'>
+      <input
+        className='z-20 block w-full rounded border border-gray-300 border-s-gray-50 bg-gray-800 p-2.5 text-sm focus:border-blue-500 focus:ring-blue-500'
+        placeholder='Search posts by userId'
+        onChange={onSearchChange}
+        value={term}
+      />
+    </div>
   );
 }
